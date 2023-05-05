bd2ef7440c5c8359f0717773ce46912dd65b7579
zbekxz <forestnatplay@yandex.ru>
2023-05-05 08:46:51 +0600

    adding necessary methods

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -17,4 +17,28 @@ public class MyHashTable<K, V> {
     private int M = 11;
     private int size;
 
+    public MyHashTable() {
+
+    }
+    public MyHashTable(int M) {
+
+    }
+    private int hash(K key) {
+
+    }
+    public void put(K key, V value) {
+
+    }
+    public V get(K key) {
+
+    }
+    public V remove(K key) {
+
+    }
+    public boolean contains(V value) {
+
+    }
+    public K getKey(V value) {
+
+    }
 }
