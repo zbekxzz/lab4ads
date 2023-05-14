393e51aeff6d7d2f245b6c4d93cf01ffad6b3a2d
zbekxz <forestnatplay@yandex.ru>
2023-05-15 04:51:52 +0600

    adding documentation in inner class HashNode

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,22 +1,48 @@
 import java.util.LinkedList;
 public class MyHashTable<K, V> {
-    private class HashNode<K, V> {
-        private K key;
-        private V value;
-
+    private class HashNode<K, V> { // inner class necessary to create hash table
+        private K key; // field to store key
+        private V value; // field to store value
+        /**
+         * HashNode - constructor to set values in fields
+         * @param key - given key to set
+         * @param value - given value to set
+         * @return void
+         */
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+        /**
+         * getKey - method that returns key of node
+         * @noparam
+         * @return K - key type
+         */
         public K getKey() {
             return key;
         }
+        /**
+         * getValue - method that returns value of node
+         * @noparam
+         * @return V - value type
+         */
         public V getValue() {
             return value;
         }
+        /**
+         * setValue - method that set value in node
+         * @param value - given value to set
+         * @return void
+         */
         public void setValue(V value) {
             this.value = value;
         }
+
+        /**
+         * toString - method that returns node with specific type
+         * @noparam
+         * @return String
+         */
         public String toString() {
             return "{" + key + " " + value + "}";
         }
