64119798facfab3f66b556dcc36f5d74273e568c
zbekxz <forestnatplay@yandex.ru>
2023-05-15 04:24:47 +0600

    modifying class

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,30 +1,39 @@
+import java.util.LinkedList;
 public class MyHashTable<K, V> {
     private class HashNode<K, V> {
         private K key;
         private V value;
-        private HashNode<K, V> next;
 
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
-
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
+        public void setValue(V value) {
+            this.value = value;
+        }
         public String toString() {
             return "{" + key + " " + value + "}";
         }
     }
-    private HashNode<K, V>[] chainArray;
+    private LinkedList<HashNode<K, V>>[] chainArray;
     private int M = 11;
     private int size;
 
     public MyHashTable() {
-
+        this.size = 0;
+        this.chainArray = new LinkedList[M];
     }
     public MyHashTable(int M) {
 
     }
     private int hash(K key) {
-
+        return key.hashCode() % M;
     }
     public void put(K key, V value) {
 
